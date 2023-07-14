20d8fa3c7ebbd36c1aad2208bd7777236da6959f
Juan Navas <iam@juannavas.dev>
2023-07-14 18:03:01 -0400

    Test and code alert for server error response

---
+++ sundaes-on-demand/src/pages/entry/tests/OrderEntry.test.jsx
@@ -0,0 +1,27 @@
+import { render, screen, waitFor } from '@testing-library/react';
+import OrderEntry from '../OrderEntry';
+import { rest } from 'msw';
+import { server } from '../../../mocks/server';
+
+test('Handles error for scoops and toppings routes', async () => {
+  server.resetHandlers(
+    rest.get('http://localhost:3030/scoops', (req, res, ctx) => {
+      res(ctx.status(500));
+    }),
+    rest.get('http://localhost:3030/toppings'),
+    (req, res, ctx) => {
+      res(ctx.status(500));
+    }
+  );
+
+  render(<OrderEntry />);
+
+  await waitFor(async () => {
+    const alerts = await screen.findAllByRole('alert');
+    // const alerts = await screen.findAllByText(
+    //   /An unexpected error occurred. Please try again later./i
+    // );
+
+    expect(alerts).toHaveLength(2);
+  });
+});
